6696c591fceadca75a8725964dc34a1ca5aabc3c
jillian <jilliandanielle@gmail.com>
2020-10-27 16:10:34 +0000

    added tests for block cards

--- cypress/integration/blog_spec.js
+++ cypress/integration/blog_spec.js
@@ -7,5 +7,17 @@ describe('Page Test', () => {
           })
         cy.get('h1').contains('Views From The Academy')
     })
+
+    it('Checks content of blog card' , () => {
+        cy.visit({
+            method:'GET',
+            url:'/',
+            failOnStatusCode: false
+          })
+
+        cy.get(':nth-child(2) > .card > .card-body > .card-text').contains('Some quick example')
+        cy.get(':nth-child(3) > .card > .card-body > .card-text').contains('Some quick example')
+        cy.get(':nth-child(4) > .card > .card-body > .card-text').contains('Some quick example')
+    })
 })
 
